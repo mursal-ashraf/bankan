03b5b2975689e2012f6fe6e9bc7144b3dfc3e635
Mursal Ashraf <mash0019@student.monash.edu>
2023-08-10 21:59:17 +1000

    adding basic home page component

---
+++ frontend/src/components/common/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Grid } from '@mui/material';
+
+export const Footer: React.FC = () => (
+  <Grid
+    container
+    justifyContent="end"
+    alignItems="center"
+    spacing={2}
+    component="footer"
+    style={{ borderTop: 'double' }}
+  >
+    <Grid item>
+      <Button color="inherit">Terms of Use</Button>
+    </Grid>
+    <Grid item>
+      <Button color="inherit">Privacy policy</Button>
+    </Grid>
+  </Grid>
+);
