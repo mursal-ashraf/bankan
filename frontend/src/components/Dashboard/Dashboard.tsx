b0025dc47026cad889a1081230461fd870861337
Imeth Don <ikah0003@student.monash.edu>
2023-08-12 00:18:26 +1000

    scrollable projects list (Imeth)

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,95 @@
 import { Tile } from "@/common/tiles/ContentTile";
-import React from "react";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
-const BoardContainer: React.FC = ({ children }: any) => {
+interface BoardCardProps {
+  project: string;
+  description: string;
+  lastModified: string;
+}
+
+//Mock data... remove once we fetch.
+const boardCards: BoardCardProps[] = [
+  {
+    project: "Project A",
+    description: "This is the description for Project A",
+    lastModified: "2023-08-10",
+  },
+  {
+    project: "Project B",
+    description: "Description for Project B goes here",
+    lastModified: "2023-08-09",
+  },
+  {
+    project: "Project C",
+    description: "Description for Project C",
+    lastModified: "2023-08-08",
+  },
+  {
+    project: "Project A",
+    description: "This is the description for Project A",
+    lastModified: "2023-08-10",
+  },
+  {
+    project: "Project B",
+    description: "Description for Project B goes here",
+    lastModified: "2023-08-09",
+  },
+  {
+    project: "Project C",
+    description: "Description for Project C",
+    lastModified: "2023-08-08",
+  },
+  {
+    project: "Project A",
+    description: "This is the description for Project A",
+    lastModified: "2023-08-10",
+  },
+  {
+    project: "Project B",
+    description: "Description for Project B goes here",
+    lastModified: "2023-08-09",
+  },
+  {
+    project: "Project C",
+    description: "Description for Project C",
+    lastModified: "2023-08-08",
+  },
+];
+
+const BoardCardElement: React.FC<BoardCardProps> = ({
+  project,
+  description,
+  lastModified,
+}) => {
   return (
-    <div className="flex flex-col mx-5 my-10 rounded-xl bg-white w-2/3">
-      <h1 className="text-black m-5">Dashboard</h1>
+    <div className="flex w-full my-3 justify-center">
+      <div className="rounded-lg shadow-md bg-slate-300 p-6 w-11/12">
+        <h2 className="text-xl font-semibold mb-2">{project}</h2>
+        <p className="text-gray-700">{description}</p>
+        <p className="text-gray-500 mt-2">Last Modified: {lastModified}</p>
+      </div>
+    </div>
+  );
+};
 
-      <p className="text-black m-5">Search for a board</p>
-      <div className="flex w-full my-3 h-2/3 justify-center">
-        <div className="mx-5 rounded-xl bg-slate-300 w-10/12 text-center">
-          hello
-        </div>
+const BoardContainer: React.FC = () => {
+  return (
+    <div className="flex flex-col mx-5 my-10 py-3 rounded-xl bg-white w-2/3 items-center">
+      <div className="mx-10 my-5 p-2 rounded-3xl shadow-md bg-slate-300 w-2/3 self-start">
+        Search...
+      </div>
+
+      <div className="flex flex-col w-full my-3 overflow-y-auto">
+        {boardCards.map((boardCard, index) => (
+          <BoardCardElement key={index} {...boardCard} />
+        ))}
+      </div>
+
+      <div className="flex my-5 w-1/3 h-20">
+        <Button className="w-full" variant="contained" startIcon={<AddIcon />}>
+          Create New
+        </Button>
       </div>
     </div>
   );
