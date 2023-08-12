d867707b532eec6a9375c1053738ee2e69ea0aee
Dongyi Wang <dwan0067@student.monash.edu>
2023-08-12 17:41:46 +1000

    Profile -> Update with MUI material
    Add new background color as the Figma is, add new MUI TextFields and replace the old ones.

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
+import { TextField } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 import Button from '@mui/material/Button';
 
 export const Profile: React.FC = () => {
-
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -24,7 +24,7 @@ export const Profile: React.FC = () => {
     };
 
     return (
-        <div className="absolute inset-0 h-screen w-screen flex items-center justify-center bg-gray-100">
+        <div className="absolute inset-0 h-screen w-screen flex items-center justify-center" style={{ backgroundColor: '#FFCD29' }}>
             <div className="bg-white p-8 rounded-lg shadow-md w-4/5 h-4/5">
                 <div className="animate-pulse flex h-full">
                     <div className="relative w-1/4 h-1/4 flex flex-col items-center">
@@ -41,12 +41,54 @@ export const Profile: React.FC = () => {
                         </Button>
                     </div>
                     <div className="ml-6 flex flex-col justify-between w-3/4 space-y-4">
-                        <input name="name" value={formData.name} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Name" />
-                        <input name="email" value={formData.email} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Email" />
-                        <input name="phone" value={formData.phone} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Phone" />
-                        <input name="address" value={formData.address} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Address" />
-                        <input name="company" value={formData.company} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Company" />
-                        <input name="description" value={formData.description} onChange={handleInputChange} className="h-20 bg-gray-300 rounded w-3/4 text-xl text-black pl-4" placeholder="Description" />
+                        <TextField 
+                            name="name"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                            label="Name"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <TextField 
+                            name="email"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            label="Email"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <TextField 
+                            name="phone"
+                            value={formData.phone}
+                            onChange={handleInputChange}
+                            label="Phone"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <TextField 
+                            name="address"
+                            value={formData.address}
+                            onChange={handleInputChange}
+                            label="Address"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <TextField 
+                            name="company"
+                            value={formData.company}
+                            onChange={handleInputChange}
+                            label="Company"
+                            variant="standard"
+                            fullWidth
+                        />
+                        <TextField 
+                            name="description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                            label="Description"
+                            variant="standard"
+                            fullWidth
+                        />
                     </div>
                 </div>
             </div>
