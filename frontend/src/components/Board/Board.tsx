8f8cd77e78aa733509c283da2735ad2c8d35f62c
Imeth Don <ikah0003@student.monash.edu>
2023-08-13 16:42:25 +1000

    page styling fixes (Imeth)

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,66 +1,67 @@
-import { useState } from "react";
+import { useState } from 'react';
 // import { AccountCircle } from "@mui/icons-material";
-import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
+import { Tile } from '@/common/ContentTile';
 
 // Mock data
 const board = {
-  id: "123456789",
+  id: '123456789',
   version: 1,
   saved_date: new Date().toDateString(),
   created_at: new Date().toDateString(),
-  user_id: "1111",
-  team_id: "2222",
-  name: "FIT3162 Board",
+  user_id: '1111',
+  team_id: '2222',
+  name: 'FIT3162 Board',
   description:
-    "This board is used to monitor the progress of the FIT3162 CS_20 Project.",
+    'This board is used to monitor the progress of the FIT3162 CS_20 Project.',
 };
 
 // list table
 const columns: IColumn[] = [
   {
-    id: "1",
-    board_id: "123456789",
+    id: '1',
+    board_id: '123456789',
     board_version: 1,
     index: 1,
-    name: "Backlog",
+    name: 'Backlog',
     created_at: new Date().toDateString(),
     cards: [
       {
-        id: "1",
-        list_id: "1",
+        id: '1',
+        list_id: '1',
         index: 1,
-        user_creator: "1",
+        user_creator: '1',
         user_assigned: undefined,
-        title: "Design the UI!",
+        title: 'Design the UI!',
         description:
-          "Create a figma design of the initial UI to demo to the clients.",
+          'Create a figma design of the initial UI to demo to the clients.',
         deadline: undefined,
         created_at: new Date().toDateString(),
       },
     ],
   },
   {
-    id: "2",
-    board_id: "123456789",
+    id: '2',
+    board_id: '123456789',
     board_version: 1,
     index: 2,
-    name: "Doing",
+    name: 'Doing',
     cards: [
       {
-        id: "2",
-        list_id: "2",
+        id: '2',
+        list_id: '2',
         index: 1,
-        user_creator: "1",
-        user_assigned: "1",
-        title: "Get to know the team!",
-        description: "Do ice breaker activities to understand the team better.",
+        user_creator: '1',
+        user_assigned: '1',
+        title: 'Get to know the team!',
+        description: 'Do ice breaker activities to understand the team better.',
         deadline: new Date().toDateString(),
         created_at: new Date().toDateString(),
       },
@@ -68,39 +69,39 @@ const columns: IColumn[] = [
     created_at: new Date().toDateString(),
   },
   {
-    id: "3",
-    board_id: "123456789",
+    id: '3',
+    board_id: '123456789',
     board_version: 1,
     index: 3,
-    name: "Code Review",
+    name: 'Code Review',
     cards: [],
     created_at: new Date().toDateString(),
   },
   {
-    id: "4",
-    board_id: "123456789",
+    id: '4',
+    board_id: '123456789',
     board_version: 1,
     index: 4,
-    name: "Testing",
+    name: 'Testing',
     cards: [],
     created_at: new Date().toDateString(),
   },
   {
-    id: "5",
-    board_id: "123456789",
+    id: '5',
+    board_id: '123456789',
     board_version: 1,
     index: 5,
-    name: "Done",
+    name: 'Done',
     cards: [
       {
-        id: "3",
-        list_id: "5",
+        id: '3',
+        list_id: '5',
         index: 1,
-        user_creator: "1",
-        user_assigned: "1",
-        title: "Watch Seminar Recordings",
+        user_creator: '1',
+        user_assigned: '1',
+        title: 'Watch Seminar Recordings',
         description:
-          "Watch the Seminar recordings before meeting with the team.",
+          'Watch the Seminar recordings before meeting with the team.',
         deadline: new Date().toDateString(),
         created_at: new Date().toDateString(),
       },
@@ -136,32 +137,32 @@ const columns: IColumn[] = [
 // ];
 
 const team = {
-  id: "123123",
-  board_id: "123456789",
-  user_id: "1",
+  id: '123123',
+  board_id: '123456789',
+  user_id: '1',
   created_at: new Date().getDate(),
 };
 
 // user_team table
 const members = [
   {
-    id: "1",
-    email: "kev@example.com",
-    phone: "123123123",
+    id: '1',
+    email: 'kev@example.com',
+    phone: '123123123',
     raw_user_meta_data: {
-      username: "Kev",
-      given_name: "Kevin",
-      surname: "L",
+      username: 'Kev',
+      given_name: 'Kevin',
+      surname: 'L',
     }, // currently how supabase by default wants to store user data
   },
   {
-    id: "2",
-    email: "mursal@example.com",
-    phone: "123123123",
+    id: '2',
+    email: 'mursal@example.com',
+    phone: '123123123',
     raw_user_meta_data: {
-      username: "Mursal",
-      given_name: "Mursal",
-      surname: "A",
+      username: 'Mursal',
+      given_name: 'Mursal',
+      surname: 'A',
     },
   },
 ];
@@ -197,17 +198,17 @@ function TaskCard(props: { item: ICard }) {
   // console.log(item)
   return (
     <>
-      <Card className="m-1" sx={{ maxWidth: 345 }}>
+      <Card className='m-1' sx={{ maxWidth: 345 }}>
         <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
+          <Typography gutterBottom variant='h6' component='div'>
             {item.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant='body2' color='text.secondary'>
             {item.description}
           </Typography>
         </CardContent>
-        <CardActions className="flex flex-col">
-          <Button className="justify-self-end" size="small">
+        <CardActions className='flex flex-col'>
+          <Button className='justify-self-end' size='small'>
             Edit
           </Button>
         </CardActions>
@@ -224,31 +225,35 @@ function TaskColumn(props: IColumnProp) {
   // console.log("Column: ", {column}, {items})
   return (
     <>
-      <div className="flex flex-col h-min-full w-full min-w-[200px] mx-2 px-2 bg-gray-500 rounded-md pt-2">
-        <div className="w-full bg-gray-100 text-black font-bold text-xl rounded-md text-center border-2 border-gray-700">
+      <div className='flex flex-col h-min-full w-full min-w-[200px] mx-2 px-2 bg-gray-500 rounded-md pt-2'>
+        <div className='w-full bg-gray-100 text-black font-bold text-xl rounded-md text-center border-2 border-gray-700'>
           {column.name}
         </div>
-        <div className="w-full h-full overflow-y-auto">
-        <Droppable droppableId={column.id}>
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef} className="min-h-[95%]">
-              {items.map((item: ICard, index: number) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <TaskCard item={item} />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+        <div className='w-full h-full overflow-y-auto'>
+          <Droppable droppableId={column.id}>
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className='min-h-[95%]'
+              >
+                {items.map((item: ICard, index: number) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <TaskCard item={item} />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </div>
       </div>
     </>
@@ -275,10 +280,18 @@ function TaskBoard() {
       // If moving cards in the same column
       if (result?.source?.droppableId == result?.destination?.droppableId) {
         columnSource?.cards?.splice(result?.source?.index, 1);
-        columnDestination?.cards?.splice(result?.destination?.index, 0, cardToMove);
+        columnDestination?.cards?.splice(
+          result?.destination?.index,
+          0,
+          cardToMove
+        );
       } else {
         // else moving cards between columns
-        columnDestination?.cards?.splice(result?.destination?.index, 0, cardToMove);
+        columnDestination?.cards?.splice(
+          result?.destination?.index,
+          0,
+          cardToMove
+        );
         columnSource?.cards?.splice(result?.source?.index, 1);
       }
       const newColumn = [...columns];
@@ -288,9 +301,9 @@ function TaskBoard() {
 
   return (
     <>
-      <div className="bg-gray-600 p-2 h-full overflow-x-auto">
+      <div className='bg-gray-600 p-2 h-full overflow-x-auto'>
         <DragDropContext onDragEnd={onDragEnd}>
-          <div className="h-full flex flex-row">
+          <div className='h-full flex flex-row'>
             {myColumns.map((col: IColumn, index: number) => (
               <TaskColumn column={col} key={index} />
             ))}
@@ -304,24 +317,23 @@ function TaskBoard() {
 export const Board: React.FC = () => {
   return (
     <>
-      <div
-        className="absolute inset-0 h-full w-full flex flex-col items-center justify-center"
-        style={{ backgroundColor: "#FFCD29" }}
-      >
-        <div className="w-[95%] h-[90%] flex flex-col items-center justify-center">
-          <p className="bg-white text-black text-bold px-10 text-4xl font-mono font-bold m-4 rounded-md">
-            {board.name}
-          </p>
+      <Tile colour='yellow' height={100}>
+        <div className=' inset-0 h-full w-full flex flex-col items-center '>
+          <div className='w-[95%] h-[90%] flex flex-col items-center justify-center'>
+            <p className='bg-white text-black text-bold px-10 text-4xl font-mono font-bold m-4 rounded-md'>
+              {board.name}
+            </p>
 
-          <div className="bg-white w-full m-4 p-2 rounded-md text-black font-bold">
-            Members
-          </div>
+            <div className='bg-white w-full m-4 p-2 rounded-md text-black font-bold'>
+              Members
+            </div>
 
-          <div className="bg-white p-2 md:p-6 rounded-md shadow-md w-full h-full">
-            <TaskBoard />
+            <div className='bg-white p-2 md:p-6 rounded-md shadow-md w-full h-full'>
+              <TaskBoard />
+            </div>
           </div>
         </div>
-      </div>
+      </Tile>
     </>
   );
 };
