b5c57262e27fe83a137012aeaffe3b464f791be7
KevinLu9 <88708641+KevinLu9@users.noreply.github.com>
2023-08-15 00:20:51 +1000

    initial board page commit (#7)
    * added initial skeleton for board page
    
    * Merge branch 'develop' into board-page
    
    * page styling fixes (Imeth)
    
    * cleaned up types
    
    ---------
    
    Co-authored-by: Imeth Don <ikah0003@student.monash.edu>

--- frontend/src/common/commons.d.ts
+++ frontend/src/common/commons.d.ts
@@ -1,6 +1,33 @@
-type GeneralColour = "yellow" | "blue" | "white" | "orange";
+type GeneralColour = 'yellow' | 'blue' | 'white' | 'orange';
 
 interface TextAndAction {
   text: string;
   action: () => void;
+}
+
+// Board Types
+interface IColumn {
+  id: string;
+  board_id: string;
+  board_version: number;
+  index: number;
+  name: string;
+  cards: ICard[];
+  created_at: string;
+}
+
+interface ICard {
+  id: string;
+  list_id: string | undefined;
+  index: number;
+  user_creator: string;
+  user_assigned: string | undefined;
+  title: string;
+  description: string;
+  deadline: string | undefined;
+  created_at: string;
+}
+
+interface IColumnProp {
+  column: IColumn;
 }
