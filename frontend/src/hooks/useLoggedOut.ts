818ed4016687c5d6d3f52c221ed1a5c7bb1937f1
Dongyi Wang <dwan0067@student.monash.edu>
2023-08-15 15:23:41 +1000

    Create useLoggedOut.ts

---
+++ frontend/src/hooks/useLoggedOut.ts
@@ -0,0 +1,31 @@
+import { useState, useCallback } from 'react';
+import { AuthError } from '@supabase/supabase-js';
+import { useClient } from '@/contexts/AppContext';
+
+interface LogOutResult {
+  isLoading: boolean;
+  error: AuthError | undefined | null;
+}
+
+type LogOutHookFunction = () => Promise<void>;
+
+export type LogOutHook = () => [LogOutHookFunction, LogOutResult];
+
+export const useLogOut: LogOutHook = () => {
+  const client = useClient();
+  const [result, setResult] = useState<LogOutResult>({
+    isLoading: false,
+    error: undefined,
+  });
+
+  const performLogOut = useCallback<LogOutHookFunction>(
+    async () => {
+      setResult({ ...result, isLoading: true });
+      await client.auth.signOut();
+      setResult({ isLoading: false, error: null });
+    },
+    [client, result]
+  );
+
+  return [performLogOut, result];
+};
