977cafaef1094d07ea658269539481edb11b250d
Mursal Ashraf <89443356+mursal-ashraf@users.noreply.github.com>
2023-08-16 21:46:44 +1000

    adding schema types from supabase (#14)

--- frontend/src/utils/supabase/supabaseClient.ts
+++ frontend/src/utils/supabase/supabaseClient.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'schema';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
+const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
 export const getSupabaseClient = () => {
   return supabaseClient;
 };
