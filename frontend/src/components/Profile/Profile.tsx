e75eb99bdc81ccd27b86ab3eb16df171a20ec491
Dongyi Wang <100513480+Eeyore6@users.noreply.github.com>
2023-08-16 19:58:08 +1000

    Feature/profile Picture Uploading (#10)
    * Update Profile.tsx
    
    Add skeleton Profile UI, add a profile image, edit button, and information blocks(blocks have titles and can be editable, the data cannot be saved)
    
    * Profile -> Update with MUI material
    
    Add new background color as the Figma is, add new MUI TextFields and replace the old ones.
    
    * Profile Update Edit Button
    
    Add a new pop-up window to the edit button which users could use to upload/replace pics
    
    * Profile Adjustments
    
    Resize the components
    
    * Profile.tsx Update
    
    Remove the console.log in Function handleUploadClick().
    
    * Profile Upload Picture function
    
    The uploaded picture will correctly be shown in the page.
    
    * Update Profile.tsx
    
    Add }; at the end
    
    ---------
    
    Co-authored-by: Mursal Ashraf <89443356+mursal-ashraf@users.noreply.github.com>

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,148 +1,141 @@
 import React, { useState } from 'react';
-import {
-  TextField,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-} from '@mui/material';
+import { TextField, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 
 export const Profile: React.FC = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    address: '',
-    company: '',
-    description: '',
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        company: '',
+        description: ''
+    });
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [image, setImage] = useState<string | null>(null);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
 
-  const handleUploadClick = () => {
-    setOpen(false);
-  };
+    const handleUploadClick = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div
-      className="absolute inset-0 h-screen w-screen flex items-center justify-center"
-      style={{ backgroundColor: '#FFCD29' }}
-    >
-      <div className="bg-white p-8 rounded-lg shadow-md w-4/5 h-4/5">
-        <div className="flex h-full">
-          <div className="relative w-1/3 h-1/3 flex flex-col items-center justify-center">
-            <div className="w-40 h-40 rounded-full bg-gray-300 flex items-center justify-center">
-              <AccountCircle style={{ width: 160, height: 160 }} />
+    return (
+        <div className="absolute inset-0 h-screen w-screen flex items-center justify-center" style={{ backgroundColor: '#FFCD29' }}>
+            <div className="bg-white p-8 rounded-lg shadow-md w-4/5 h-4/5">
+                <div className="flex h-full">
+                    <div className="relative w-1/3 h-1/3 flex flex-col items-center justify-center">
+                    <div className="w-40 h-40 rounded-full bg-gray-300 flex items-center justify-center">
+                    {image ? (
+                    <img src={image} alt="Profile" className="w-40 h-40 rounded-full" />
+                    ) : (
+                        <AccountCircle style={{ width: 160, height: 160 }} />
+                        )}
+                    </div>
+                        <div className="pt-4"></div>
+                        <Button 
+                            variant="contained" 
+                            color="primary" 
+                            className="mt-6 text-xl"
+                            style={{padding: '8px 24px'}}
+                            onClick={() => setOpen(true)}>
+                            Edit
+                        </Button>
+                    </div>
+                    <div className="relative ml-12 flex flex-col w-3/5 space-y-2">
+                        <label className="text-2xl font-bold text-black">Name</label>
+                        <TextField 
+                            name="name"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                            label="Name"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                        />
+                        <label className="text-2xl font-bold text-black">Email</label>
+                        <TextField 
+                            name="email"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            label="Email"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                                                    />
+                        <label className="text-2xl font-bold text-black">Phone Number</label>
+                        <TextField 
+                            name="phone"
+                            value={formData.phone}
+                            onChange={handleInputChange}
+                            label="Phone Number"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                                                    />
+                        <label className="text-2xl font-bold text-black">Address</label>
+                        <TextField 
+                            name="address"
+                            value={formData.address}
+                            onChange={handleInputChange}
+                            label="Address"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                        />
+                        <label className="text-2xl font-bold text-black">Company</label>
+                        <TextField 
+                            name="company"
+                            value={formData.company}
+                            onChange={handleInputChange}
+                            label="Company"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                        />
+                        <label className="text-2xl font-bold text-black">Description</label>
+                        <TextField 
+                            name="description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                            label="Description"
+                            variant="filled"
+                            fullWidth
+                            InputProps={{style: {fontSize: 20, height: '3rem'}}}
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="pt-4"></div>
-            <Button
-              variant="contained"
-              color="primary"
-              className="mt-6 text-xl"
-              style={{ padding: '8px 24px' }}
-              onClick={() => setOpen(true)}
-            >
-              Edit
-            </Button>
-          </div>
-          <div className="relative ml-12 flex flex-col w-3/5 space-y-2">
-            <label className="text-2xl font-bold text-black">Name</label>
-            <TextField
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              label="Name"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-            <label className="text-2xl font-bold text-black">Email</label>
-            <TextField
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              label="Email"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-            <label className="text-2xl font-bold text-black">
-              Phone Number
-            </label>
-            <TextField
-              name="phone"
-              value={formData.phone}
-              onChange={handleInputChange}
-              label="Phone Number"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-            <label className="text-2xl font-bold text-black">Address</label>
-            <TextField
-              name="address"
-              value={formData.address}
-              onChange={handleInputChange}
-              label="Address"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-            <label className="text-2xl font-bold text-black">Company</label>
-            <TextField
-              name="company"
-              value={formData.company}
-              onChange={handleInputChange}
-              label="Company"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-            <label className="text-2xl font-bold text-black">Description</label>
-            <TextField
-              name="description"
-              value={formData.description}
-              onChange={handleInputChange}
-              label="Description"
-              variant="filled"
-              fullWidth
-              InputProps={{ style: { fontSize: 30, height: '3rem' } }}
-            />
-          </div>
+            
+            {/* Dialog for image upload */}
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>Upload or Replace Picture</DialogTitle>
+                <DialogContent>
+                    <input type="file" accept="image/*" onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        if (file) {
+                            // Handle the file here if needed
+                            const reader = new FileReader();
+                            reader.onloadend = () => {
+                                setImage(reader.result as string);
+                            };
+                            reader.readAsDataURL(file);
+                        }
+                    }} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleUploadClick} color="primary">
+                        Upload
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
-      </div>
-
-      {/* Dialog for image upload */}
-      <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>Upload or Replace Picture</DialogTitle>
-        <DialogContent>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-              const file = e.target.files?.[0];
-              if (file) {
-                // Handle the file here if needed
-              }
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleUploadClick} color="primary">
-            Upload
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+    );
 };
