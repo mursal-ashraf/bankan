0447e0b0fef0625286246a9ebbb76f91f5faa082
KevinLu9 <kevin.lu902@gmail.com>
2023-09-04 23:59:09 +1000

    added loading/error state for api board fetching

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -6,6 +6,8 @@ import EditCardModal from './BoardComponents/EditCardModal';
 import ComponentContainer from '../common/ComponentContainer';
 import useBoard from '@/hooks/useBoard';
 import { useParams } from 'react-router-dom';
+import { Button, LinearProgress } from '@mui/material';
+import { Refresh } from '@mui/icons-material';
 
 // eslint-disable-next-line
 const team = {
@@ -40,7 +42,8 @@ const InnerBoard: React.FC = () => {
   // http://localhost:5173/Board/02b5bfae-f543-42b6-aab0-51b39253a2e4
   const { board_id } = useParams() as { board_id: string };
 
-  const { data, isLoading, error, refetch, isRefetching } = useBoard(board_id);
+  const { data, isLoading, isError, refetch, isRefetching } =
+    useBoard(board_id);
 
   const [currentEditCard, setCurrentEditCard] = useState<ICard>();
   const [editModalVisibility, setEditModalVisibility] = useState(false);
@@ -54,29 +57,33 @@ const InnerBoard: React.FC = () => {
     setEditModalVisibility(!editModalVisibility.valueOf());
   };
 
-  if (isLoading || isRefetching) {
-    return <>Loading fellas!!!!!</>;
-  }
-
-  if (error) {
-    return (
-      <>
-        <h1>Error</h1>
-        <button onClick={() => refetch()}>Retry</button>
-      </>
-    );
-  }
-
   const [board] = (data || []).slice(-1) || [{}];
 
   return (
     <>
+      {(isLoading || isRefetching) && <LinearProgress color="secondary" />}
+      {isError && (
+        <LinearProgress variant="determinate" color="error" value={100} />
+      )}
       <Tile colour="yellow" height={93} className="">
         <div className=" inset-0 h-full w-full flex flex-col items-center">
+          <div className="w-full flex flex-col items-center justify-center">
+            {isError && (
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => refetch()}
+              >
+                Reload Board
+                <Refresh className="lr-1" />
+              </Button>
+            )}
+          </div>
           <div className="w-[95%] h-[90%] flex flex-col items-center justify-center">
             <p className="bg-white text-black text-bold px-10 text-4xl font-mono font-bold m-4 rounded-md">
               {board?.name}
             </p>
+
             <MemberBar members={members} />
             <div className="bg-white p-2 md:p-6 rounded-md shadow-md w-full h-full overflow-auto">
               <TaskBoard board={board} onEditCardSelect={onEditCardClick} />
