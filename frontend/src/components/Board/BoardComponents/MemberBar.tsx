f34f89073c81e3936af3ce191bccb995c3cc4f3b
Mursal Ashraf <mash0019@student.monash.edu>
2023-09-19 14:43:01 +1000

    simplify query + cleanups

--- frontend/src/components/Board/BoardComponents/MemberBar.tsx
+++ frontend/src/components/Board/BoardComponents/MemberBar.tsx
@@ -32,7 +32,7 @@ export function MemberBar() {
         <p className="mx-2">Members</p>
         {(users || []).map((member) => (
           <div className="mx-1">
-            <MemberIcon member={member as any} />
+            <MemberIcon member={member} />
           </div>
         ))}
         <IconButton onClick={() => navigateTo(`/Board/member/${board_id}`)}>
