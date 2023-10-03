0eee40b22674b44be72ad8d1343f0cbdab889953
Mursal Ashraf <mash0019@student.monash.edu>
2023-10-03 11:18:01 +1100

    removing cache time

--- frontend/src/hooks/useBoard.ts
+++ frontend/src/hooks/useBoard.ts
@@ -1,12 +1,14 @@
 import { useTypedSupabaseQuery } from './utils';
 
 const useBoard = (board_id: string) => {
-  return useTypedSupabaseQuery((supabase) =>
-    supabase
-      .from('board')
-      .select()
-      .eq('id', board_id)
-      .order('version', { ascending: true }),
+  return useTypedSupabaseQuery(
+    (supabase) =>
+      supabase
+        .from('board')
+        .select()
+        .eq('id', board_id)
+        .order('version', { ascending: true }),
+    { cacheTime: 0 },
   );
 };
 
