47118672fa89850fb08c772ab7348d5d3adf66f4
Mursal Ashraf <mash0019@student.monash.edu>
2023-10-07 12:00:47 +1100

    showing owner of the board (Mursal)

--- frontend/src/components/Board/BoardComponents/MemberBar.tsx
+++ frontend/src/components/Board/BoardComponents/MemberBar.tsx
@@ -12,7 +12,7 @@ export function MemberBar() {
     supabase
       .from('board')
       .select(
-        'id, version, name, created_at, saved_date, team_id, team (id, user_team (user_id, member(*)))',
+        'id, version, name, created_at, saved_date, team_id, team (id, user_team (user_id, member(*))), user_id, member (id, name, email)',
       )
       .eq('id', board_id)
       .order('version', { ascending: true }),
@@ -27,11 +27,14 @@ export function MemberBar() {
       ?.team?.user_team?.map((user_team) => user_team.member),
   );
 
+  const owner = data?.find((board) => board.id === board_id)?.member as any;
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading board</div>;
   return (
     <>
       <div className="flex flex-row items-center w-full h-16 bg-white m-4 p-2 rounded-md text-black font-bold">
+        <p className="mx-2">Created by: {owner?.name}</p>
         <p className="mx-2">Members</p>
         {(users || []).map((member) => (
           <div key={member.id} className="mx-1">
